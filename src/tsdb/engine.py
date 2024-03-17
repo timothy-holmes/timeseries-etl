370fb39412b1ca1622121590a2165975cddcd1f2
Timothy Holmes <tim.a.holmes@gmail.com>
2024-03-17 16:27:33 +1100

    tighten up Engine API and tests

--- src/tsdb/engine.py
+++ src/tsdb/engine.py
@@ -1,5 +1,6 @@
-from queue import Queue
+from queue import Queue, Empty
 import threading
+import time
 
 from tinyflux import TinyFlux, Point
 
@@ -7,40 +8,43 @@ from tinyflux import TinyFlux, Point
 class Engine:
     def __init__(self, tinyflux_path):
         self._tinyflux_path = tinyflux_path
-        self.queue = Queue()
         self._worker_thread = None
         self._exit_worker = False
-
-    def _start_worker(self):
-        self.exit_worker = False
-        self.worker_thread = threading.Thread(target=self._worker)
-        self.worker_thread.start()
-        return True
-
-    def _stop_worker(self):
-        self.exit_worker = True
-        if self.worker_thread:
-            self.worker_thread.join()
-        return True
+        self._queue = Queue()
 
     def _worker(self):
         with TinyFlux(self._tinyflux_path) as db:
-            while not self.exit_worker:
-                if not self.queue.empty():
-                    item = self.queue.get()
-                    db.insert(item)
+            while not self._exit_worker:
+                if not self._queue.empty():
+                    try:
+                        item = self._queue.get(timeout=1)
+                    except Empty:
+                        pass
+                    else:
+                        db.insert(item)
 
-    def _worker_alive(self):
-        if self.worker_thread:
-            return self.worker_thread.is_alive()
+    def _is_worker_alive(self):
+        if self._worker_thread:
+            return self._worker_thread.is_alive()
         else:
             return False
 
+    def start_worker(self):
+        self._exit_worker = False
+        if not self._is_worker_alive():
+            self._worker_thread = threading.Thread(target=self._worker)
+            self._worker_thread.start()
+        return True
+
+    def stop_worker(self):
+        self._exit_worker = True
+        if self._is_worker_alive():
+            self._worker_thread.join()
+        return True
+
     def queue_size(self):
-        return self.queue.qsize()
+        return self._queue.qsize()
 
     def insert(self, item: Point):
-        self.queue.put(item)
+        self._queue.put(item)
 
-    def close(self):
-        self._stop_worker()
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,24 +1,31 @@
 from datetime import datetime, timedelta
 import time
+import random
+import string
 
 from tinyflux import Point
 
 from tsdb.engine import Engine
 
-TEST_TINYFLUX_PATH = "./tests/data/test.db"
+def temp_tinyflux_path():
+    r = ''.join(
+        random.choices(
+            string.ascii_lowercase + string.digits, 
+            k=8
+        )
+    )
+    return f"./tests/data/{r}-temp.db"
 
 
 def test_worker_starts_and_stops():
-    engine = Engine(TEST_TINYFLUX_PATH)
-    engine._start_worker()
-    assert engine._worker_alive() is True
-    engine._stop_worker()
-    assert engine._worker_alive() is False
-    engine.close()
-
+    engine = Engine(temp_tinyflux_path())
+    engine.start_worker()
+    assert engine._is_worker_alive() is True
+    engine.stop_worker()
+    assert engine._is_worker_alive() is False
 
 def test_insert_and_queue_size():
-    engine = Engine(TEST_TINYFLUX_PATH)
+    engine = Engine(temp_tinyflux_path())
     point1 = Point(
         time=datetime.now() - timedelta(seconds=1),
         measurement="measurement1",
@@ -37,14 +44,37 @@ def test_insert_and_queue_size():
 
     assert engine.queue_size() == 2
 
-    engine._start_worker()
+    engine.start_worker()
     time.sleep(1)
 
     assert engine.queue_size() == 0
 
+    engine.stop_worker()
+
 
 def test_close():
-    engine = Engine(TEST_TINYFLUX_PATH)
-    engine._start_worker()
-    engine.close()
-    assert engine._worker_alive() == False
+    engine = Engine(temp_tinyflux_path())
+    engine.start_worker()
+    assert engine._is_worker_alive() is True
+    engine.stop_worker()
+    assert engine._is_worker_alive() is False
+
+
+def test_performance():
+    pre = time.time()
+    engine = Engine(temp_tinyflux_path())
+    engine.start_worker()
+    points = [Point(
+        time=datetime.now() + timedelta(seconds=i),
+        measurement="measurement2",
+        tags={"tag2": "value2"},
+        fields={"field2": random.randint(0, 100)},
+    ) for i in range(1000)]
+    pre_end = time.time() - pre
+    for point in points:
+        engine.insert(point)
+    while engine.queue_size() > 0:
+        time.sleep(0.1)
+    end = time.time() - pre
+    engine.stop_worker()
+    assert False, f'{pre} {pre_end}, {end}'
